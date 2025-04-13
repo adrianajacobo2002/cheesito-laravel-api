38a38c3ca85fa3a41e7e717d72475614ae2c7617
Emilia Escobar <emiliaescobar105@gmail.com>
2025-04-12 18:39:36 -0600

    PDF stock

---
+++ config/dompdf.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'font_dir' => resource_path('fonts/'),
+    'font_cache' => storage_path('fonts/'),
+    'default_font' => 'poppins',
+    'fonts' => [
+        'poppins' => [
+            'R' => 'Poppins-Regular.ttf',
+        ],
+        'quicksand' => [
+            'B' => 'Quicksand-Bold.ttf',
+        ],
+    ],
+];
+
